f8eec3b6fd7ea78e80283a2d027153d2fb345063
Liang <liangosc@gmail.com>
2026-06-18 10:23:54 -0700

    Create gameinfos.inc.php to resolve BGA inclusion error

---
+++ bga_rps/gameinfos.inc.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * ------
+ * BGA gameinfos.inc.php for rps
+ * ------
+ */
+
+$gameinfos = array( 
+    'designer' => 'Liang',
+    'artist' => 'Liang',
+    'year' => 2026,
+    'publisher' => 'Indie',
+    'publisher_website' => '',
+    'publisher_bgg_id' => 0,
+    'bgg_id' => 0,
+    'players' => array( 2 ),
+    'suggest_player_number' => null,
+    'not_recommend_player_number' => null,
+    'estimated_duration' => 5,
+    'fast_additional_time' => 30,
+    'medium_additional_time' => 40,
+    'slow_additional_time' => 50,
+    'tie_breaker_description' => '',
+    'losers_not_sorted' => false,
+    'strategy' => 3,
+    'diplomacy' => 1,
+    'player_colors' => array( "00f2fe", "ff007f" ),
+    'favorite_colors_support' => true,
+    'disable_player_order_display' => false,
+    'game_interface_width' => array(
+        'min' => 740,
+        'max' => null
+    ),
+    'is_sandbox' => true,
+    'turn_control_support' => true,
+    'tags' => array( 2, 23 )
+);
